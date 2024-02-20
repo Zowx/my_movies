d01042956512441ddf478314ab08ca4374d0730f
Zowx <angles.coude.0f@icloud.com>
2024-02-20 11:01:12 +0100

    J2

---
+++ src/Form/NewMovieType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use App\Entity\User;
+use Doctrine\DBAL\Types\BooleanType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewMovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('dateRelease', DateTimeType::class)
+            ->add('type', TextType::class)
+            ->add('synopsis', TextType::class)
+            ->add('realisator', TextType::class)
+            ->add('see', BooleanType::class)
+            ->add('createdAt', DateTimeType::class)
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
